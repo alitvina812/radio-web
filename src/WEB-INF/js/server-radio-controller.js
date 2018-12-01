dca63e8d8f3a900d4c04e6fad4758f913ac71f3d
Anastasiia Litvina <al@click-solutions.de>
2018-12-01 16:33:45 +0100

    add server radio controller

---
+++ src/WEB-INF/js/server-radio-controller.js
@@ -0,0 +1,29 @@
+"use strict";
+
+(function() {
+    const Controller = de_sb_radio.Controller;
+
+    const ServerRadioController = function() {
+        Controller.call(this);
+    }
+    ServerRadioController.prototype = Object.create(Controller.prototype);
+    ServerRadioController.prototype.constructor = ServerRadioController;
+
+    Object.defineProperty(ServerRadioController.prototype, "display", {
+        enumerable: false,
+        configurable: false,
+        writable: true,
+        value: function() {
+            
+            console.log(Controller.sessionOwner)
+            const mainElement = document.querySelector("main");
+            mainElement.appendChild(document.querySelector("#server-radio-template").content.cloneNode(true).firstElementChild);
+        }
+    });
+
+    window.addEventListener("load", event => {
+        const anchor = document.querySelector("header li:nth-of-type(2) > a");
+        const controller = new ServerRadioController();
+        anchor.addEventListener("click", event => controller.display());
+    })
+} ());
