e14df6aa65297d7e05c7e202bd9a22f18f7d91ac
Anastasiia Litvina <al@click-solutions.de>
2018-12-06 23:20:10 +0100

    add random playlist

--- src/WEB-INF/js/server-radio-controller.js
+++ src/WEB-INF/js/server-radio-controller.js
@@ -13,11 +13,69 @@
         enumerable: false,
         configurable: false,
         writable: true,
-        value: function() {
-            
+        value: function () {            
             console.log(Controller.sessionOwner)
             const mainElement = document.querySelector("main");
             mainElement.appendChild(document.querySelector("#server-radio-template").content.cloneNode(true).firstElementChild);
+            this.startPlaylist();
+        }
+    });
+
+    Object.defineProperty(ServerRadioController.prototype, "startPlaylist", {
+        value: function () {
+            let audioElement = document.getElementById("audio-element");
+            let startButton = document.getElementById("start-playlist");
+            
+            const audioContext = new AudioContext();
+            const track = audioContext.createMediaElementSource(audioElement);
+            // track.connect(audioContext.destination);
+
+            startButton.addEventListener("click", () => {
+
+                if (audioContext.state === 'false') {
+                    audioContext.resume();
+                }
+
+                if (startButton.dataset.playing === 'false') {
+                    audioElement.play();
+                    startButton.dataset.playing = 'true';
+                } else if (startButton.dataset.playing === 'true') {
+                    audioElement.pause();
+                    startButton.dataset.playing = 'false';
+                }
+            }, false);
+
+            const addressString = "../../services/documents/";
+            const trackIds = [5, 8, 9, 10, 11, 12];
+
+            audioElement.addEventListener("ended", function () {
+                // startButton.dataset.playing = 'false';
+                    let randomTrack = trackIds[Math.floor(Math.random() * trackIds.length)];
+                    console.log(randomTrack);
+                    audioElement.src = addressString + randomTrack;
+                    audioElement.play();
+
+            },  false);
+
+            // volume
+            const gainNode = audioContext.createGain();
+            track.connect(gainNode).connect(audioContext.destination);
+            const volumeControl = document.getElementById("volume");
+            volumeControl.addEventListener("input", function () {
+                gainNode.gain.value = this.value;
+            }, false);
+
+        }
+    });
+
+    Object.defineProperty(ServerRadioController.prototype, "getGenres", {
+        value: async function () {
+            // let sessionOwner = Controller.sessionOwner;
+            try {
+                genres = JSON.parse(await this.xhr("/services/tracks/genres", "GET", {"Accept": "application/json"}, "", "text"));
+            } catch (error) {
+                this.displayError(error);
+            }
         }
     });
 
@@ -25,5 +83,5 @@
         const anchor = document.querySelector("header li:nth-of-type(2) > a");
         const controller = new ServerRadioController();
         anchor.addEventListener("click", event => controller.display());
-    })
+    });
 } ());
